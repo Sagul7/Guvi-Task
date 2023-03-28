12983cdf101d151058e2095b5b9e5ce2507c9489
Sagul7 <118523385+Sagul7@users.noreply.github.com>
2023-03-28 14:53:15 +0530

    Add files via upload

---
+++ guvi_ass/php/sign.php
@@ -0,0 +1,51 @@
+<?php
+
+$Username = $_POST['Username'];
+$Gmail    = $_POST['Gmail'];
+$Age      = $_POST['Age'];
+$Password = $_POST['Password'];
+$Confrompass =$_POST['Confrompass'];
+
+if(!empty($Username) || !empty($Gmail) || !empty($Age) || !empty($Password) || !empty($Confrompass))
+{
+    $host = "localhost";
+    $dbusername = "root";
+    $dbpassword = "";
+    $dbname = "guvi";
+
+ $conn =new mysqli($host,$dbusername,$dbpassword,$dbname);
+
+if (mysqli_connect_error()){
+    die('Connect Error ('. mysqli_connect_errno() .') '
+      . mysqli_connect_error());
+  }
+  else{
+   $SELECT = "SELECT Gmail From signup Where Gmail = ? Limit 1";
+   $INSERT = "INSERT Into signup (Username , Gmail ,Age,Password,Confrompass )values(?,?,?,?,?)";
+   
+  //Prepare statement
+       $stmt = $conn->prepare($SELECT);
+       $stmt->bind_param("s", $Gmail);
+       $stmt->execute();
+       $stmt->bind_result($Gmail);
+       $stmt->store_result();
+       $rnum = $stmt->num_rows;
+   
+       //checking username
+        if ($rnum==0) {
+        $stmt->close();
+        $stmt = $conn->prepare($INSERT);
+        $stmt->bind_param("ssiss",$Username,$Gmail,$Age,$Password,$Confrompass);
+        $stmt->execute();
+        echo "New record inserted sucessfully";
+       } else {
+        echo "Someone already register using this email";
+       }
+       $stmt->close();
+       $conn->close();
+      }
+  } else {
+  echo "All field are required";
+  die();
+  }
+  ?>
